25f532fd89b6158261d9cbec974b783ba2cf136a
dragonkeep <531780561@qq.com>
2025-10-25 15:47:55 +0800

    add base config for GitHub Pages

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,16 +3,19 @@ import { fileURLToPath, URL } from "node:url";
 import { getSidebar } from "./utils/getSidebar";
 
 export default defineConfig({
+
   // 标签上显示的网站标题
   title: "Easy VitePress Blog",
   titleTemplate: "Blog",
+  
   // 在标签上显示所的 logo
   head: [["link", { rel: "icon", href: "/logo.svg" }]],
 
   // 网站描述，有利于被搜索引擎捕获
-  description:
+  description:  
     "zbwer's tech blog: An undergraduate's journey through frontend development, sharing insights, tips, and experiences in web technologies.",
-
+  // 网站部署的基础路径
+  base: '/', 
   // md 文件根目录
   // 【谨慎修改】：一旦修改将引起较多变动
   srcDir: "./src",
